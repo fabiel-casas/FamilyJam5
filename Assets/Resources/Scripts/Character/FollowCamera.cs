1ddb4a1bf288725965df4e3c6610334767e2e8c3
fabiel016 <fabiel016_@hotmail.com>
2015-10-24 16:17:10 -0500

    new scripts
    camera script
    movement and animation script

---
+++ Assets/Resources/Scripts/Character/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+  public Transform target;
+  public float distance = 5;
+  public float movementSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+    target = GameObject.FindGameObjectWithTag(StaticTags.PlayerTag).GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if(target != null) {      
+      transform.position = Vector3.Lerp(transform.position, target.position, movementSpeed) + new Vector3(0, 0, -distance);
+    }
+	}
+}
---
+++ Assets/Resources/Scripts/Maze/PlayerMovementsMaze.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementsMaze : MonoBehaviour {
+
+  #region PUBLIC_VARIABLES
+  public float speed = 5;
+  #endregion
+
+  #region PPROTECTED_VARIABLES
+  protected Rigidbody mRigidBody;
+  #endregion
+
+  // Use this for initialization
+  void Start () {
+    if(mRigidBody == null) {
+      mRigidBody = GetComponent<Rigidbody>();
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    float horizontal = Input.GetAxis("Horizontal");    float vertical = Input.GetAxis("Vertical");
+
+    Vector3 direction = new Vector3(horizontal, vertical, 0);
+
+    mRigidBody.AddRelativeForce(direction * speed);
+    
+  }
+}
