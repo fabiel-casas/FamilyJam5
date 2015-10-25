d6035a26f08a9c4102d89225af016f6d6dd82bdd
fabiel016 <fabiel016_@hotmail.com>
2015-10-24 21:16:44 -0500

    level complete

--- Assets/Resources/Scripts/Maze/PlayerMovementsMaze.cs
+++ Assets/Resources/Scripts/Maze/PlayerMovementsMaze.cs
@@ -21,10 +21,7 @@ public class PlayerMovementsMaze : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     float horizontal = Input.GetAxis("Horizontal");    float vertical = Input.GetAxis("Vertical");
-
     Vector3 direction = new Vector3(horizontal, vertical, 0);
-
     mRigidBody.AddRelativeForce(direction * speed);
-    
   }
 }
